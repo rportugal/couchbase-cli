0467668162111a735f6dee2d7b6abc9a884aeb6d
Ricardo Portugal <ricardofmportugal@gmail.com>
2019-05-22 12:06:02 +0100

    get/query; handle config

--- bin/couchbase-cli.js
+++ bin/couchbase-cli.js
@@ -1,27 +1,76 @@
 #! /usr/bin/env node
+const { Couchbase } = require('couchbase-js');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const os = require('os');
+const path = require('path');
 const program = require('commander');
-const rp = require('request-promise');
 
-program.version('0.1.0');
+program.version(process.env.npm_package_version);
 
+const configPath = path.join(os.homedir(), '.couchbase-cli.json');
+
+const loadConfig = () => {
+  const rawData = fs.readFileSync(configPath);
+  return JSON.parse(rawData);
+};
+
+const saveConfig = config => {
+  fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+};
+
+const bucket = () => {
+  const config = loadConfig();
+  const currentConfig = config.configs[config.current];
+  const { url, username, password, insecure } = currentConfig;
+  const conn = new Couchbase(url, username, password, { insecure });
+  return conn.openBucket('digital');
+};
+
+program
+  .command('switch')
+  .description('switch to another config')
+  .action(async function() {
+    const config = loadConfig();
+    const choices = config.configs.map((conf, idx) => ({ key: idx.toString(), name: conf.name, value: idx }));
+    await inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: 'Select config: ',
+          name: 'switch',
+          choices
+        }
+      ])
+      .then(answers => {
+        config.current = answers.switch;
+        saveConfig(config);
+      });
+  });
 program
   .command('get <documentId>')
   .description('get a document')
-  .action(function(req) {
-    console.log('get ');
-    rp('http://www.google.com')
-      .then(res => console.log(res))
-      .catch(err => console.error(err));
+  .action(async function(documentId) {
+    try {
+      const data = await bucket().get(documentId);
+      console.log(JSON.stringify(data, null, 2));
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
   });
 
 program
   .command('query <n1ql>')
   .description('run a N1QL query')
-  .action(function(n1ql) {
-    console.log(`query ${n1ql}`);
-    rp('http://www.google.com')
-      .then(res => console.log(res))
-      .catch(err => console.error(err));
+  .action(async function(n1ql) {
+    try {
+      const data = await bucket().query(n1ql);
+      console.log(JSON.stringify(data, null, 2));
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
   });
 
 program
@@ -33,5 +82,3 @@ program
     }
   });
 program.parse(process.argv);
-
-// load config
